82b5929019c20d9b762b816dd0be5fa63ecd7194
Pooja <poojabansal87@gmail.com>
2018-11-09 19:43:21 -0800

    Adding bonus component

--- static/js/app.js
+++ static/js/app.js
@@ -13,11 +13,82 @@ function buildMetadata(sample) {
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
     Object.entries(sampleDict).forEach(([key, value]) => {
-      meta.append("p"). text(key+" : "+value);     
+      meta.append("p"). text(key+": "+value);     
     });
-  });
+    meta.style("font-size","12px")
+        .style("font-weight","bold");
     // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+    buildGauge(sampleDict.WFREQ); 
+  });
+}
+
+function buildGauge(wfreq) {
+  // Using the value of wfreq
+  var level = wfreq;
+
+  // Trig to calc meter point
+  var degrees = (9 - level)*20,
+	radius = .5;
+  var radians = degrees * Math.PI / 180;
+  var x = radius * Math.cos(radians);
+  var y = radius * Math.sin(radians);
+
+  // Path: may have to change to create a better triangle
+  var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+	 pathX = String(x),
+	 space = ' ',
+	 pathY = String(y),
+	 pathEnd = ' Z';
+var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+var data = [{ type: 'scatter',
+   x: [0], y:[0],
+	marker: {size: 28, color:'850000'},
+	showlegend: false,
+	name: 'washes',
+	text: level,
+	hoverinfo: 'text+name'},
+  { values: [50/9, 50/9, 50/9,50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+  rotation: 90,
+  text: ['8-9', '7-8', '6-7', '5-6',
+			'4-5', '3-4', '2-3','1-2','0-1',''],
+  textinfo: 'text',
+  textposition:'inside',	  
+  marker: {colors:['rgba(30, 127, 0, .5)', 								'rgba(60, 140, 22, .5)',
+						 'rgba(90, 150, 44, .5)',
+						 'rgba(120, 160, 66, .5)',
+						 'rgba(150, 170, 88, .5)',
+						 'rgba(180, 180, 110, .5)',
+						 'rgba(202, 190, 140, .5)',
+						 'rgba(210, 206, 180, .5)', 							'rgba(232, 226, 202, .5)',
+						 'rgba(255, 255, 255, 0)']},
+  labels: ['8-9', '7-8', '6-7', '5-6',
+			'4-5', '3-4', '2-3','1-2','0-1',''],
+  hoverinfo: 'label',
+  hole: .5,
+  type: 'pie',
+  showlegend: false
+}];
+
+var layout = {
+  shapes:[{
+      type: 'path',
+      path: path,
+      fillcolor: '850000',
+      line: {
+        color: '850000'
+      }
+    }],
+  title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per week',
+  height: 500,
+  width: 500,
+  xaxis: {zeroline:false, showticklabels:false,
+			 showgrid: false, range: [-1, 1]},
+  yaxis: {zeroline:false, showticklabels:false,
+			 showgrid: false, range: [-1, 1]}
+};
+
+Plotly.newPlot('gauge', data, layout);
 }
 
 function buildCharts(sample) {
@@ -32,7 +103,7 @@ function buildCharts(sample) {
     // console.log(data.otu_ids);
     // console.log(data.otu_labels);
     // console.log(data.sample_values);
-    
+
     var plot_data = [
       {
         x: data.otu_ids, 
@@ -40,7 +111,11 @@ function buildCharts(sample) {
         type: 'scatter',
         mode: 'markers',
         marker: {
-          size: data.sample_values
+          size: data.sample_values,
+          // color: d3.scale
+          //         .linear()
+          //         .domain([0,d3.max(data.sample_values)])
+          //         .range(["#50f442", "#f44141"])
         },
         text: data.otu_ids.map(function(d,i){
           return "("+d+", "+data.sample_values[i]+")"+data.otu_labels[i];
@@ -89,7 +164,7 @@ function buildCharts(sample) {
       }
     ];
     var layout = {
-      height: 400,
+      height: 500,
       width: 500
     };
     Plotly.newPlot("pie", pieplot_data, layout);
